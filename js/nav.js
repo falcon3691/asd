2767c7298a0c63e084a63d2ec6ecd212589fdf30
falcon3691 <72970681+falcon3691@users.noreply.github.com>
2024-08-25 16:50:31 +0300

    aslantesvik.com sitesinin dosyaları eklendi.

---
+++ js/nav.js
@@ -0,0 +1,8 @@
+function fonk1(){
+const menu4 = document.querySelector('.sidebar');
+if (menu4.style.display == 'flex') {
+        menu4.style.display = 'none';
+    } else {
+        menu4.style.display = 'flex';
+    }
+}
